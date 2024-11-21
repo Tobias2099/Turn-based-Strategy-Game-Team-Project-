af02cd5e9ce681cc78bcc182061fc5b8be495002
Tobias <tpong@uwaterloo.ca>
2024-11-21 16:57:46 -0500

    add board.h

---
+++ board.h
@@ -0,0 +1,28 @@
+#ifndef __BOARD_H__
+#define __BOARD_H__
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Board{
+    size_t width;
+    size_t height;
+    vector<vector<int>> boardrep;
+    vector<AbstractEntity*> pieces;
+
+    public:
+      Board(size_t width, size_t height);
+      ~Board();
+      void addthing2board(Piece* bt);
+
+      int appearance2id(char c);
+      bool simplemove(string playerincontrol, int id, char dir);
+      Piece* whoat(int x, int y);
+      Piece* whoat(int id);
+};
+
+//int battle(Piece* initiator, Piece* defender);
+
+#endif
