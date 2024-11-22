b59211fbceec9adc35a8e89e4f6f4cf6b3f133a6
anubhavsengupta <anubhavakki2016@gmail.com>
2024-11-21 20:48:17 -0500

    fix mc

--- game.cc
+++ game.cc
@@ -299,30 +299,8 @@ int Game::battle(AbstractLink* initiator, AbstractLink* defender) {
   }
 }
 
-AbstractEntity* whoAt(int x, int y) {
-  int id = b->getValue(x, y);
-  if (id == -1) {
-      return nullptr;
-  }
 
-  return pieces[id];
-}
 
-AbstractEntity* whatAt(int x, int y) {
-  int id = getValue(x, y);
-
-  if (id == -1) {
-      return nullptr;
-  }
-
-  Piece* thepiece = pieces[id];
-  return thepiece;
-}
-
-AbstractEntity* whatAt(int id) {
-  Piece* thepiece = pieces[id];
-  return thepiece;
-}
 AbstractEntity* whoAt(int x, int y) {
   int id = b->getValue(x, y);
   if (id == -1) {
